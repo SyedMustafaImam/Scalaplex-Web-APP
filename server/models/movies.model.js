742a91bfd8a3596e9881638c70bff74eb5b64c5d
Muskan Punshi <muskanpunshi9@gmail.com>
2021-05-31 12:01:49 +0500

    movie model

---
+++ server/models/movies.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose'),
+const Schema= mongoose.Schema;
+
+let Movies = new Schema({
+    movieid:{type:Number,required:true},
+    moviename:{type:String,required:true},
+    theaterno:{type:String,required:true},
+    seats:{type:String,required:true},
+    time:{type:String,required:true},
+    date:{type:Date,default:Date.now}
+
+
+})
+module.exports = mongoose.model('Movie',Movies);
