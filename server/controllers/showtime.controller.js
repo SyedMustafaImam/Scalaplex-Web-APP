1119dbc68e8f8dc58716d1872e749942b737dab9
Syed Mustafa Imam <cs1812134@szabist.pk>
2021-05-31 22:20:08 +0500

    Revert "done work on show time and others"
    This reverts commit 7fe98e0a238b17670508b9d1e1d0b646c9877d36.

--- server/controllers/showtime.controller.js
+++ server/controllers/showtime.controller.js
@@ -51,14 +51,3 @@ exports.getmovie_showtime=(req,res)=>{
         res.status(500).json({error:"Something Went Wrong"})
     })
 }
-exports.list_showtime=(req,res)=>{
-    db.Showtime.find()
-    .then(result=>{
-        console.log(result)
-        res.status(200).json({message:"Working"})
-    })
-    .catch(err=>{
-        console.log(err)
-        res.status(500).json({error:"Something Went Wrong"})
-    })
-}
